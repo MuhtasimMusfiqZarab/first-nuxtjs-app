d696e246311b66b5339b271c2202347d823a9e97
MacBook Air <hello@DESKTOP-D9KD6TQ.localdomain>
2019-09-02 14:04:05 +0600

    adding dynamic route

---
+++ pages/users/_id/index.vue
@@ -0,0 +1,4 @@
+<template>
+  <!-- $route is provided by vue js by the vue router giving us the information with the currently loaded route -->
+  <div>This is single user route for : {{$route.params.id}}</div>
+</template>
